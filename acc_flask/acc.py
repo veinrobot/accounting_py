ac172a821caa697a1767ca4812ac2b180da43fbf
JingShing <JingShingLai@gmail.com>
2023-12-26 20:55:15 +0800

    remove edit test url

--- acc_flask/acc.py
+++ acc_flask/acc.py
@@ -206,10 +206,6 @@ def showdata():
     user_records = get_user_records()
     return render_template('showdata.html', records=user_records)
 
-@app.route('/edit', methods=['POST'])
-def edit():
-    pass
-
 if __name__ == '__main__':
     port_number = 5000
 
